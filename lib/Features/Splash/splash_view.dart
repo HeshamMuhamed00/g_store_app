8de51fc4d57ff387c91b1f3b9100cf12d983d98a
HeshamMohamed00 <heshammuh@gmail.com>
2024-01-22 14:35:06 -0800

    create splash view

---
+++ lib/Features/Splash/splash_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:g_store_app/Features/Splash/widget/splash_view_body.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
