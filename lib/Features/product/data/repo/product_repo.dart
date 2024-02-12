dcc5d217cb6ebf07286d3031e42c97522e0887f9
HeshamMohamed00 <heshammuh@gmail.com>
2024-02-12 11:39:50 -0800

    create product repo

--- lib/Features/product/data/repo/product_repo.dart
+++ lib/Features/product/data/repo/product_repo.dart
@@ -1,3 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:g_store_app/Core/Errors/failure.dart';
+import 'package:g_store_app/Features/product/data/models/product_model/product_model.dart';
+
 abstract class ProductRepo {
-  fetchProduct();
+  Future<Either<Failure, List<ProductModel>>> fetchProduct();
+  Future<Either<Failure, List<ProductModel>>> fetchSearchProduct();
 }
