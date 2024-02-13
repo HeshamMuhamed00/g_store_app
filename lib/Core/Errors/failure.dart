76d12ba5ec4e84c0dd8814695d890f17f9f870c2
HeshamMohamed00 <heshammuh@gmail.com>
2024-02-13 08:49:58 -0800

    fetch product request is done

--- lib/Core/Errors/failure.dart
+++ lib/Core/Errors/failure.dart
@@ -1,1 +1,52 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String errMessage;
+
+  Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with api server');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with api server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with api server');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad certificate');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioException.response!.statusCode!, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure('Request api server was canceld');
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection failed');
+      case DioExceptionType.unknown:
+        if (dioException.message!.contains('SocketException')) {
+          return ServerFailure('No internet connection');
+        }
+        return ServerFailure('unexpected error , please try later');
+
+      default:
+        return ServerFailure(
+            'Opps there was an error , please try again later!');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your request not found , please try later');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal server error , please try later');
+    } else {
+      return ServerFailure('Opps there was an error , please try again ');
+    }
+  }
+}
---
+++ lib/Features/product/data/repo/product_repo_impl.dart
@@ -0,0 +1,36 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:g_store_app/Core/Errors/failure.dart';
+import 'package:g_store_app/Core/utils/api_service.dart';
+import 'package:g_store_app/Features/product/data/models/product_model/product_model.dart';
+import 'package:g_store_app/Features/product/data/repo/product_repo.dart';
+
+class ProductRepoImpl implements ProductRepo {
+  final ApiService apiService;
+
+  ProductRepoImpl(this.apiService);
+  @override
+  Future<Either<Failure, List<ProductModel>>> fetchProduct() async {
+    try {
+      var data = await apiService.get(endPoint: 'products');
+
+      List<ProductModel> products = [];
+
+      products.add(
+        ProductModel.fromJson(data),
+      );
+      return Right(products);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<ProductModel>>> fetchSearchProduct() {
+    // TODO: implement fetchSearchProduct
+    throw UnimplementedError();
+  }
+}
